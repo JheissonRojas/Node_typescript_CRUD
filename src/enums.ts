3617e8dcdb4924e145cc9bbe029ee126df4ec77b
Jheisson Rojas <ingeniero.scrummers4@interrapidisimo.com>
2023-03-03 00:27:25 -0500

    type definition and MongoDB configuration

---
+++ src/enums.ts
@@ -0,0 +1,11 @@
+export enum brands{
+    Honda = 'Honda',
+    Yamaha = 'Yamaha',
+    Susuki = 'Susuki',
+    Bmw = 'Bmw',
+    Ducati = 'Ducati'
+}
+
+export enum Motor{
+    
+}
---
+++ src/models/motorcicles.ts
@@ -0,0 +1,17 @@
+import { prop, Typegoose  } from "typegoose";
+ 
+class motorcicle extends Typegoose{
+  
+  @prop({required: true, trim: true, minlength: 3}) // mongoose properties
+  country?: string //typescript data types
+  @prop({min: 1000, max: 8000, required: true}) 
+  engine?: number
+  @prop({min: 1990, max: 2023, required: true}) 
+  year?: number
+  @prop({required: true, trim: true, minlength: 3}) 
+  brand?: string
+  
+}
+
+const MotorcicleModel = new motorcicle().getModelForClass(motorcicle)
+export default MotorcicleModel
